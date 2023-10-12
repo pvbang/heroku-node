07ec7095054dcb1e4d28b38fd47e71b5fd1bf435
pvbang <pvbang23092002@gmail.com>
2023-10-12 13:57:40 +0700

    update

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/views'));
 
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
-
-// set the home page route
 app.get('/', function(req, res) {
-
-	// ejs render automatically looks in the views folder
-	res.render('index');
+    // Sử dụng path.join để xác định đường dẫn tới tệp index.html
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 app.listen(port, function() {
-	console.log('Our app is running on http://localhost:' + port);
+    console.log('Our app is running on http://localhost:' + port);
 });
